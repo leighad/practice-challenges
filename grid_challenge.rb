63bf77e2a68d7087830659e9bf0c54cf328d9575
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-08-03 17:21:17 -0700

    solve by brute force

--- grid_challenge.rb
+++ grid_challenge.rb
@@ -1,19 +1,31 @@
 # grid_setup = [[row_1], [row_2], [row_3]]
-test_grid = [["inactive", "inactive", "inactive"], ["inactive", "active", "inactive"], ["inactive", "inactive", "inactive"]]
+test_grid_1 = [["inactive", "inactive", "inactive"], ["inactive", "active", "inactive"], ["inactive", "inactive", "inactive"]]
+test_grid_2 = [["active", "active", "inactive"], ["inactive", "active", "inactive"], ["inactive", "inactive", "inactive"]]
+test_grid_3 = [["active", "active", "active"], ["inactive", "active", "inactive"], ["inactive", "inactive", "inactive"]]
+test_grid_4 = [["active", "active", "active"], ["inactive", "active", "inactive"], ["inactive", "active", "inactive"]]
 
 
-def count_active_cells(grid)
-    count = 0
-    grid.each do |row| 
-        row.each do |el|
-            unless el === grid[1][1]
-                if el === "active"
-                    count += 1
-                end
-            end
-        end
+# def count_active_cells(grid)
+#     count = 0
+#     grid.each do |row| 
+#         row.each do |el|
+#             count += 1 if (el == "active" && el != grid[1][1]) 
+#         end
+#     end
+#     count
+# end
+
+def count_active_cells(grid_array)
+    counter = 0
+    grid_array.each do |row|
+      row.each do |square|
+        counter += 1 if (square == 'active') 
+      end
     end
-    count
+    counter - 1
 end
 
-p count_active_cells(test_grid)
+p count_active_cells(test_grid_1)
+p count_active_cells(test_grid_2)
+p count_active_cells(test_grid_3)
+p count_active_cells(test_grid_4)
